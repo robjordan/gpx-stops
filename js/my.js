d5d15e09bb15b6195290a0dda7382aca50d973fa
robjordan <github@jordan-maynard.com>
2017-01-22 21:52:04 +0000

    Multiple

--- js/my.js
+++ js/my.js
@@ -1,8 +1,12 @@
 document.getElementById('fileinput').addEventListener('change', function(){
-    var file = this.files[0];
-    // This code is only for demo ...
-    console.log("name : " + file.name);
-    console.log("size : " + file.size);
-    console.log("type : " + file.type);
-    console.log("date : " + file.lastModified);
+    for(var i = 0; i<this.files.length; i++){
+        var file =  this.files[i];
+        // This code is only for demo ...
+        console.group("File "+i);
+        console.log("name : " + file.name);
+        console.log("size : " + file.size);
+        console.log("type : " + file.type);
+        console.log("date : " + file.lastModified);
+        console.groupEnd();
+    }
 }, false);
