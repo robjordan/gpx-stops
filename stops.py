eb5a5c58db3cbbddb90d4d4634bd3f003c8666fc
robjordan <github@jordan-maynard.com>
2017-02-01 12:15:16 +0000

    refactor

--- stops.py
+++ stops.py
@@ -11,7 +11,7 @@ from sklearn import preprocessing
 from geojsonio import display
 from geojson import dumps,  Feature, Point, FeatureCollection, LineString
 
-def cluster_by_k_means (frame, k=100):
+def cluster_by_k_means(frame, k=100):
     # Mean Normalize features
     scaler = preprocessing.StandardScaler()
     scaler.fit(frame[['x', 'y', 'altitude', 'time']])
@@ -25,17 +25,83 @@ def cluster_by_k_means (frame, k=100):
     centroids = scaler.inverse_transform(kmeans_model.cluster_centers_)
     print("OK", len(centroids), "centroids found",
         file=sys.stderr, end="\n", flush=True)
-
-    # print("Searching for stops...", file=sys.stderr, end="\n", flush=True)
-    # for c in range(0, len(centroids)):
-    #     # count how many points within 100m of the centroid
-    #     cluster = frame.iloc[labels == c]
-    #     d = cluster[['x', 'y']] - [centroids[c, 0], centroids[c, 1]]
-    #     near = d[(abs(d['x']) < 100) & (abs(d['y']) < 100)]
-    #     if len(near.index) > min_pts:
-    #         lon, lat = proj(
-    #             centroids[c, 0], centroids[c, 1], inverse=True)
     return centroids, labels
+
+
+def cluster_by_interpoint_distance(frame, max_d=50, min_time=60, min_pts=3):
+    i = 0
+    stops = []
+    duration = 0
+    # Parse each point, and see if the following point is within max_d metres
+    # If so, and other minimum conditions are met, label it as Stopped
+    while (i < len(frame.index) - 1):
+        t0 = frame.iloc[i]['time']
+        lat0 = frame.iloc[i]['latitude']
+        lon0 = frame.iloc[i]['longitude']
+        x0 = frame.iloc[i]['x']
+        y0 = frame.iloc[i]['y']
+        i0 = i
+        d2 = 0
+
+        while (d2 < max_d ** 2) and (i < len(frame.index) - 1):
+            # print(".", file=sys.stderr, end="", flush=True)
+            i = i + 1
+            d2 = (
+                frame.iloc[i]['x'] - x0) ** 2 + (frame.iloc[i]['y'] - y0) ** 2
+            duration = (frame.iloc[i]['time'] - t0)
+
+        # print("\n", file=sys.stderr, end="", flush=True)
+
+        if (i - i0 > min_pts) and (duration > min_time):
+            for t in range(i0, i):
+                frame.set_value(frame.index[t], 'stopped', 1)
+
+    # Loop once more through the list,
+    # aggregating any adjacent stopped segments
+    prev = False
+    for i in range(0, len(frame.index)):
+        row = frame.iloc[i]
+        if (row['stopped'] != prev):   # change of state
+            if prev:                            # from Stopped to Started
+                # output previous run of Stops
+                duration = (frame.iloc[i - 1]['time'] - t0)
+                x = frame.iloc[i0:i - 1]['x'].mean()
+                y = frame.iloc[i0:i - 1]['y'].mean()
+
+                stops.append({'x': x, 'y': y, 'duration': duration})
+            else:                               # from Started to Stopped
+                # start capturing duration and stats
+                t0 = row['time']
+                i0 = i
+            prev = row['stopped']
+
+    # if track ended in Stopped state output the remaining items
+    if prev:
+        duration = (frame.iloc[i - 1]['time'] - frame.iloc[i0]['time'])
+        x = frame.iloc[i0:i - 1]['x'].mean()
+        y = frame.iloc[i0:i - 1]['y'].mean()
+        stops.append({'x': x, 'y': y, 'duration': duration})
+
+    print("# stops:", len(stops), file=sys.stderr, end="\n", flush=True)
+    return frame, stops
+
+
+def geojson_points_from_frame(gjs, fr, color):
+    for i in range(0, len(fr.index)):
+        row = fr.iloc[i]
+        gjs.append(
+            Feature(
+                geometry=Point((row['longitude'], row['latitude'])),
+                properties={
+                    "time": row['time'],
+                    "marker-color": color,
+                    "marker-symbol": \
+                        "cross" if row['stopped'] else "circle"
+                }
+            )
+        )
+#    print("# features added:", len(fr), file=sys.stderr, end="\n", flush=True)
+    return gjs
 
 
 ## MAIN ##
@@ -98,119 +164,83 @@ original = frame
 frame = frame.resample(freq).mean().interpolate(method='linear')
 print("OK\n", file=sys.stderr, end="", flush=True)
 
-# print("Reconverting lat/lon...", file=sys.stderr, end="", flush=True)
-# for i in range(0, frame.index.size):
-#     p = frame.iloc[i]
-#     # lon/lat can't be safely interpolated so refresh them here
-#     p.longitude, p.latitude = proj(p.x, p.y, inverse=True)
-# print("OK\n", file=sys.stderr, end="", flush=True)
+stops = {}
+color = {'raw': '#ff0000', 'resampled': '#00ff00'}
+x_offset = {'raw': 0, 'resampled': 100}
+original, stops['raw'] = cluster_by_interpoint_distance(
+    original, max_d, min_time, min_pts)
+frame, stops['resampled'] = cluster_by_interpoint_distance(
+    frame, max_d, min_time, min_pts)
 
-# Let's make features: distance^2 (dX^2 + dY^2) at +1, +2, +3, +4 min
-# all_features = {}
-# n = 4
+plt.hist(
+    [
+        [s['duration'] for s in stops['raw']],
+        [s['duration'] for s in stops['resampled']]
+    ],
+    bins=50,
+    color=[color['raw'], color['resampled']],
+    log=True
+)
 
-# for i in range(0, frame.index.size):
-#     features = []
-#     for f in range(1, n + 1):
-#         if (i + f < frame.index.size):
-#             features.append(
-#                 (frame.iloc[i + f].x - frame.iloc[i].x) ** 2 + (frame.iloc[i + f].y - frame.iloc[i].y) ** 2
-#             )
-#             # features.append(frame.iloc[i + f].x - frame.iloc[i].x)
-#             # features.append(frame.iloc[i + f].y - frame.iloc[i].y)
-#         else:           # zero-fill the last few slots
-#             features.append(0)
-#     all_features[frame.index[i]] = features
+# stopped = frame.loc[frame['stopped'] == True]
 
-# frame = pd.DataFrame(data=all_features)
-# frame = frame.T
-# frame.columns = ['d1', 'd2', 'd3', 'd4']
-
-# frame = frame.join(frame)
-
-
-
-# Cluster using KMeans
-# centroids, labels = cluster_by_k_means(frame, K)
-
-
-
-i = 0
-stops = []
-duration = 0
-# Run through each point, and see if the following point is within max_d metres
-# If so, and other minimum conditions are met, label it as Stopped
-while (i < len(frame.index) - 1):
-    t0 = frame.iloc[i]['time']
-    lat0 = frame.iloc[i]['latitude']
-    lon0 = frame.iloc[i]['longitude']
-    x0 = frame.iloc[i]['x']
-    y0 = frame.iloc[i]['y']
-    i0 = i
-    d2 = 0
-
-    while (d2 < max_d ** 2) and (i < len(frame.index) - 1):
-        # print(".", file=sys.stderr, end="", flush=True)
-        i = i + 1
-        d2 = (frame.iloc[i]['x'] - x0) ** 2 + (frame.iloc[i]['y'] - y0) ** 2
-        duration = (frame.iloc[i]['time'] - t0)
-
-    # print("\n", file=sys.stderr, end="", flush=True)
-
-    if (i - i0 > min_pts) and (duration > min_time):
-        for t in range(i0, i):
-            frame.set_value(frame.index[t], 'stopped', 1)
-
-# Loop once more through the list, aggregating any adjacent stopped segments
-prev = False
-for i in range(0, len(frame.index)):
-    row = frame.iloc[i]
-    if (row['stopped'] != prev):   # change of state
-        if prev:                            # from Stopped to Started
-            # output previous run of Stops
-            duration = (frame.iloc[i - 1]['time'] - t0)
-            x = frame.iloc[i0:i - 1]['x'].mean()
-            y = frame.iloc[i0:i - 1]['y'].mean()
-
-            stops.append({'x': x, 'y': y, 'duration': duration})
-        else:                               # from Started to Stopped
-            # start capturing duration and stats
-            t0 = row['time']
-            i0 = i
-        prev = row['stopped']
-
-# if track ended in Stopped state output the remaining items
-if prev:
-    duration = (frame.iloc[i - 1]['time'] - frame.iloc[i0]['time'])
-    x = frame.iloc[i0:i - 1]['x'].mean()
-    y = frame.iloc[i0:i - 1]['y'].mean()
-    stops.append({'x': x, 'y': y, 'duration': duration})
-
-stopped = frame.loc[frame['stopped'] == True]
-
-plt.scatter(x=original[['x']], y=original[['y']], marker='.')
-plt.scatter(x=stopped[['x']], y=stopped[['y']], marker='x')
-gjs_features = []
-gjs_features.append(
-    LineString([tuple(x) for x in frame[['longitude', 'latitude']].values])
-                   )
-for s in stops:
-    plt.annotate(
-        str(int(round(s['duration'] / 60, 0))),
-        (s['x'], s['y']))
-    lon, lat = proj(s['x'], s['y'], inverse=True)
-    lon = round(lon, 5)
-    lat = round(lat, 5)
-    gjs_features.append(
-        Feature(geometry=Point((lon, lat)),
-        properties={"duration": int(round(s['duration'] / 60, 0))}))
+# plt.scatter(x=original[['x']], y=original[['y']], marker='.')
+# plt.scatter(x=stopped[['x']], y=stopped[['y']], marker='x')
+# gjs_features = []
+# gjs_features.append(
+#     LineString([tuple(x) for x in frame[['longitude', 'latitude']].values])
+#                    )
+# for s in stops:
+#     plt.annotate(
+#         str(int(round(s['duration'] / 60, 0))),
+#         (s['x'], s['y']))
+#     lon, lat = proj(s['x'], s['y'], inverse=True)
+#     lon = round(lon, 5)
+#     lat = round(lat, 5)
+#     gjs_features.append(
+#         Feature(geometry=Point((lon, lat)),
+#         properties={"duration": int(round(s['duration'] / 60, 0))}))
 
 plt.show()
-display(dumps(FeatureCollection(gjs_features)), force_gist=True)
+# display(dumps(FeatureCollection(gjs_features)), force_gist=True)
+
+gjs = {}
+gjs['raw'] = []
+gjs['resampled'] = []
+
+# original data points line
+gjs['raw'].append(
+    LineString(
+        [tuple(x) for x in original[['longitude', 'latitude']].values]))
+gjs['resampled'].append(
+    LineString(
+        [tuple(x) for x in original[['longitude', 'latitude']].values]))
+
+# original/resampled points, labelled o - moving or x - stopped
+# gjs['raw'] = geojson_points_from_frame(
+#     gjs['raw'], original, color['raw'])
+# gjs['resampled'] = geojson_points_from_frame(
+#     gjs['resampled'], frame, color['resampled'])
 
 
+# detected stops
+for categ in ('raw', 'resampled'):
+    print(categ)
+    print("Number of stops:", len(stops[categ]), file=sys.stderr)
+    print("Total duration of stops (hr):",
+          sum(s['duration'] for s in stops[categ]) / 3600, file=sys.stderr)
+    for s in stops[categ]:
+        lon, lat = proj(s['x']+x_offset[categ], s['y'], inverse=True)
+        lon = round(lon, 5)
+        lat = round(lat, 5)
+        gjs[categ].append(
+            Feature(geometry=Point((lon, lat)),
+                properties={
+                    "duration": int(round(s['duration'] / 60, 0)),
+                    "marker-color": color[categ]
+                }
+            )
+        )
+    display(dumps(FeatureCollection(gjs[categ])), force_gist=True)
 
-print("Number of stops:", len(stops), file=sys.stderr)
-print("Total duration of stops (hr):",
-      sum(s['duration'] for s in stops) / 3600, file=sys.stderr)
 
